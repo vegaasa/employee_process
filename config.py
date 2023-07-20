6928e7ee844374dfd9f0cfa999f84f5f59aa334a
vegaasa <45099588+vegaasa@users.noreply.github.com>
2023-07-20 14:56:40 +0700

    Update config data
    Update config commend in code

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
+#Database Coonfiguration
 PSQL_DB = 'db_employee'
 PSQL_USER = 'postgres'
 PSQL_PORT = '5432'
 PSQL_HOST ='localhost'
 
+#File Configuration
 fl_name_employee = 'employees.csv'
 fl_name_timehseet = 'timesheets.csv'
 
